c0f1d93c964c5e396927ec590d740a064ccd023b
AnishMandal939 <mandalktaanees12@gmail.com>
2024-05-05 11:03:41 +0545

    feat - terminal setup is done in fronend : next creating bridge using socket.io

---
+++ client/src/components/Terminal.jsx
@@ -0,0 +1,32 @@
+import { Terminal as XTerminal } from "@xterm/xterm";
+import { useRef } from "react";
+import { useEffect } from "react";
+import "@xterm/xterm/css/xterm.css";
+
+export const Terminal = () => {
+  const terminalRef = useRef();
+  //   creating a flag if isRendered is true, then only render the terminal, because now it is rendering 2 times, in development mode useEffect runs 2 times and in production mode it runs 1 time
+  const isRendered = useRef(false);
+
+  useEffect(() => {
+    if (isRendered.current) return;
+    isRendered.current = true;
+    const term = new XTerminal({
+      // options
+      cursorBlink: true,
+      theme: {
+        background: "#020202",
+        foreground: "#fff",
+      },
+      rows: 20,
+    });
+    // attach terminal, and provide target , which is the ref to the div
+    term.open(terminalRef.current);
+    term.onData((data) => {
+      term.write(data);
+      console.log(data);
+    });
+    // eslint-disable-next-line
+  }, []);
+  return <div id="terminal" ref={terminalRef}></div>;
+};
--- server/Readme.md
+++ server/Readme.md
@@ -2,3 +2,10 @@
 - [ ] Create a cloud based IDE like Repl.it
 
 - bridge connection using socket.io for terminal is done
+
+# implementing terminal with front end :
+- [ ] Creating vite project
+    - xtermjs - for terminal in front end
+    - terminal setup is done 
+        Todo: next
+            creating bridge using socket.io in frontend
